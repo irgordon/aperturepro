7617b0eb7f0cac5304df4a4d37fc6f08b684522b
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-10 20:32:19 -0500

    Delete includes directory

--- includes/class-ap-geo.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace AperturePro;
-
-class Geo {
-  public static function snapshot(?string $ip): array {
-    $provider = get_option('ap_geo_provider','none');
-    return ['provider'=>$provider,'ip'=>$ip,'country'=>null,'region'=>null,'city'=>null,'lat'=>null,'lon'=>null];
-  }
-}
